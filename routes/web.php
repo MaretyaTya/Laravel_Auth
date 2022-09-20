ec12c8172d32aa5656804f8dcf28a375404904f6
maretyatya <maretyatianhar@gmail.com>
2022-09-21 00:10:12 +0700

    Authentication Moduls

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth', 'level:admin']], function() {
+    Route::get('admin', function(){
+       return 'Ini halaman Admin';
+    });
+});
+
+
+Route::group(['middleware' => ['auth', 'level:user']], function() {
+    Route::get('user', function(){
+        return 'Ini halaman User';
+    });
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
